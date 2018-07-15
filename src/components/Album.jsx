3e9ce3ed7a3ae8a5030b100f8e5ec142ad764cbc
Ruxandra Anghel <ruxiang05@gmail.com>
2018-07-15 12:28:42 +0100

    Add animation

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -17,7 +17,7 @@ class Album extends Component {
     const {id, title, link: {attributes: {href}}, 'im:image': images, 'im:price': {label: price}, 'im:itemCount': {label: tracksNumber}} = this.props;
     const {showMoreInfo} = this.state;
     const [song, artist] = title.label.split('-');
-    return <li key={id} className="column is-6">
+    return <li key={id} className="column is-6 album">
         <div className="card">
           <a href={href}>
             <div className="media is-marginless">
@@ -26,28 +26,22 @@ class Album extends Component {
                   <img src={images[2].label} alt={title} />
                 </figure>
               </div>
-              {!showMoreInfo ? 
-                <div className="media-content column is-paddingless album-title">
-                <h2 className="title is-5">{song}</h2>
+              {!showMoreInfo ? <div className="media-content column is-paddingless album-title info">
+                  <h2 className="title is-5">{song}</h2>
                   <h3 className="subtitle is-6">{artist}</h3>
-                </div>
-                :
-              <div className="media-content columns is-marginless">
-                <div className='column is-half more-info-item is-paddingless'>
-                    <h3 className='title is-4 is-marginless'>Tracks</h3>
-                    <span className='subtitle is-5'>{tracksNumber}</span>
+                </div> : <div className="media-content columns is-marginless more-info ">
+                  <div className="column is-half is-paddingless">
+                    <h3 className="title is-4 is-marginless">Tracks</h3>
+                    <span className="subtitle is-5">{tracksNumber}</span>
                   </div>
-                <div className='column is-half more-info-item is-paddingless'>
-                    <h3 className='title is-4 is-marginless'>Price</h3>
-                    <span className='subtitle is-5'>{price}</span>
+                  <div className="column is-half more-info-item is-paddingless">
+                    <h3 className="title is-4 is-marginless">Price</h3>
+                    <span className="subtitle is-5">{price}</span>
                   </div>
-                </div>
-              }
+                </div>}
             </div>
           </a>
-          <button 
-            className="button is-primary is-fullwidth is-radiusless"
-            onClick={this.moreInfoClick}>
+          <button className="button is-primary is-fullwidth is-radiusless" onClick={this.moreInfoClick}>
             More info
           </button>
         </div>
